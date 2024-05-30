f11d9e06565cc9cd44a2c9579bdba375826a934f
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-05-30 20:34:21 +0300

    Update creation.py

--- models/creation.py
+++ models/creation.py
@@ -64,9 +64,11 @@ class Creation(BaseModel, Base):
             all_posts = [{'id':i[0], 'creation_id':i[1], 'title':i[2], 'comment':i[3], 'reference':i[4],
                 'posted_at':i[5], 'fetched_at':i[6]} for i in models.storage.all_select(Post, 
                 [Post.id, Post.creation_id, Post.title, Post.comment, Post.reference, Post.posted_at, Post.fetched_at]).values()]
+            print(f"All posts for {self.name} are {len(all_posts)}")
             for post in all_posts:
                 if post['creation_id'] == self.id:
                     post_list.append(post)
+            print(f"Filtered posts for {self.name} are {len(posts_list)}")
             return sorted(post_list, key=lambda i:i.posted_at, reverse=True)
     @property
     def latest_post(self):
