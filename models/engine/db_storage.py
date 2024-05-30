7c78580880916ae9c0fb0c6d3f4d0979f565aed8
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-05-30 22:15:35 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -67,7 +67,7 @@ class DBStorage:
         new_dict = {}
         for clss in classes:
             if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).options(def(deffered)).all()
+                objs = self.__session.query(classes[clss]).options(defer(deffered)).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
