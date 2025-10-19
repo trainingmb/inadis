da2011d8ab53fa20105a609ceb897fdff0f2e68b
Your Name <you@example.com>
2025-10-20 00:08:10 +0300

    Creation backreference in progress table

--- models/user_creation.py
+++ models/user_creation.py
@@ -24,12 +24,16 @@ class UserFollowsCreator(db.Model):
     user_id = db.Column(db.String(60), db.ForeignKey('users.id'), primary_key=True)
     creator_id = db.Column(db.String(60), db.ForeignKey('creators.id'), primary_key=True)
     followed_at = db.Column(db.DateTime, default=db.func.now())
+    # relationship to Creator for convenience
+    creator = db.relationship('Creator')
 
 class UserFollowsCreation(db.Model):
     __tablename__ = 'user_follows_creation'
     user_id = db.Column(db.String(60), db.ForeignKey('users.id'), primary_key=True)
     creation_id = db.Column(db.String(60), db.ForeignKey('creations.id'), primary_key=True)
     followed_at = db.Column(db.DateTime, default=db.func.now())
+    # relationship to Creation for convenience
+    creation = db.relationship('Creation')
 
 class UserPostProgress(db.Model):
     __tablename__ = 'user_post_progress'
